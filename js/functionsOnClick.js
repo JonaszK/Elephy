01e8c3d138354aa57a4e33ab619237496b295a1c
JonaszK <43707964+JonaszK@users.noreply.github.com>
2019-10-08 11:44:53 +0200

    Add files via upload

--- js/functionsOnClick.js
+++ js/functionsOnClick.js
@@ -1,9 +1,9 @@
-var functionsCards = document.querySelectorAll('.functions__card');
-
-Array.prototype.forEach.call(functionsCards, function(el) {
-  if(window.innerWidth < 901) {
-    el.addEventListener('click', () => {
-      el.classList.contains('functions__on-click') ? el.classList.remove('functions__on-click') : el.classList.add('functions__on-click');
-    });
-  }
-});
+// var functionsCards = document.querySelectorAll('.functions__card');
+//
+// Array.prototype.forEach.call(functionsCards, function(el) {
+//   if(window.innerWidth < 901) {
+//     el.addEventListener('click', () => {
+//       el.classList.contains('functions__on-click') ? el.classList.remove('functions__on-click') : el.classList.add('functions__on-click');
+//     });
+//   }
+// });
