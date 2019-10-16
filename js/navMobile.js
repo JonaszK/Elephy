e502a28714229cffbca1321bfba51ac208f23d5a
JonaszK <43707964+JonaszK@users.noreply.github.com>
2019-10-16 20:42:59 +0200

    Add files via upload

--- js/navMobile.js
+++ js/navMobile.js
@@ -1,9 +1,18 @@
 const navSlide = () => {
   const burger = document.querySelector('.nav-mobile__burger');
   const navMobile = document.querySelector('.nav-mobile');
+  const wrapper = document.querySelector('#body--filter');
+  const l1 = document.querySelector('.line1');
+  const l2 = document.querySelector('.line2');
+  const l3 = document.querySelector('.line3');
 
   burger.addEventListener('click', () => {
     navMobile.classList.toggle('nav-mobile__active');
+    wrapper.classList.toggle('body--filter');
+    l1.classList.toggle('line1-active');
+    l2.classList.toggle('line2-active');
+    l3.classList.toggle('line3-active');
+    burger.classList.toggle('burger-active');
   });
 };
 
